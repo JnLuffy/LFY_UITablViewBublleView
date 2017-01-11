20898231a47eec18a83a8232629f643b1c06e00d
luffy <lushihong2012@163.com>
2017-01-11 20:41:07 +0800

    Initial Commit

---
+++ LFY_UITablViewBublleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LFY_UITablViewBublleView
+//
+//  Created by apple on 17/1/11.
+//  Copyright © 2017年 com.cooper.dtag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LFY_UITablViewBublleView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LFY_UITablViewBublleView
+//
+//  Created by apple on 17/1/11.
+//  Copyright © 2017年 com.cooper.dtag. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
